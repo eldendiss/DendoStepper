dc96219436671621a1b8fe25be02a15eeb5c0647
eldendiss <denis.voltmann@gmail.com>
2021-06-15 21:49:04 +0200

    Dev to release (#3)
    * add some functions
    basic documentations
    cleanup
    add status variable
    
    * added absilute positioning
    homing - needs to be checked
    fixed speed calc, need to fix acc calc
    
    * acc calc fixed
    high speeds with lenghty acceleration doesnt work, e.g when 1/32 microstepping is used
    
    * Homing verified
    4 hour run time test passed
    
    * remove main.cpp from repo

--- include/DendoStepper.h
+++ include/DendoStepper.h
@@ -6,15 +6,18 @@
 #include "stdint.h"
 #include "driver/timer.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
 #include "math.h"
 
+
+#define ENDSW_DISABLED 255
+#define HOME_ISR_DEBOUNCE 10
 /* HW configuration struct */
 typedef struct
 {
     uint8_t         step_p;         //step signal gpio
     uint8_t         dir_p;          //dir signal gpio
     uint8_t         en_p;           //enable signal gpio
+    uint8_t         endSw_p;        //endstop switch gpio, if not used set it to ENDSW_DISABLED
     timer_group_t   timer_group;    //timer group, useful if we are controlling more than 2 steppers
     timer_idx_t     timer_idx;      //timer index, useful if we are controlling 2steppers
 } DendoStepper_config_t;
@@ -27,6 +30,11 @@ enum motor_status{
     DEC,
 };
 
+enum dir{
+    CW,
+    CCW
+};
+
 typedef struct{
     uint32_t    stepInterval=2000;  //step interval in us
     uint16_t    accStepInc=100;     //step interval increase during acc/dec phase
@@ -37,6 +45,7 @@ typedef struct{
     uint16_t    speed=100;          //speed in steps*second^-1
     uint16_t    acc=100;            //acceleration in steps*second^-2
     uint8_t     status=DISABLED;
+    bool        dir=CW;
 }ctrl_var_t;
 
 class DendoStepper
@@ -44,6 +53,7 @@ class DendoStepper
 private:
     DendoStepper_config_t *conf;
     ctrl_var_t ctrl;
+    uint64_t currentPos=0;  //absolute position
     /** @brief PRIVATE: Step interval calculation
      *  @param speed maximum movement speed
      *  @param accTimeMs acceleration time in ms
@@ -70,14 +80,18 @@ private:
         return static_cast<DendoStepper *>(_this)->xISR();
     }
 
-    bool xISR();
+    static void homeISRwrap(void* _this){
+        return static_cast<DendoStepper *>(_this)->homeISR();
+    }
 
+    bool xISR();
+    void homeISR();
 
 public:
     /** @brief Costructor - prepares conf variables
      *  @param config DendoStepper_config_t pointer
      */
-    DendoStepper(DendoStepper_config_t*);
+    DendoStepper(DendoStepper_config_t* config);
     
     /** @brief initialize GPIO and Timer peripherals
      */
@@ -86,13 +100,13 @@ public:
     /** @brief runs motor to relative position in steps
      *  @param relative number of steps to run, negative is reverse 
      */
-    void runPos(int32_t);
+    void runPos(int32_t relative);
 
     /** @brief sets motor speed
      *  @param speed speed in steps per second
      *  @param accTimeMs acceleration time in ms
      */
-    void setSpeed(uint16_t,uint16_t);
+    void setSpeed(uint16_t speed,uint16_t accTimeMs);
 
     /** @brief set EN pin 1, stops movement
     */
@@ -106,6 +120,20 @@ public:
      *  @return motor_status enum
      */
     uint8_t getState();
+
+    /** @brief run motor to position in absolute coordinates (steps)
+     *  @param postition absolute position in steps from homing position (must be positive);
+     *  @return true if motor can run immediately, false if it is currently moving
+     */
+    bool runAbsolute(uint32_t position);
+
+    /** @brief homes motor, stops when stopswitch is hit and sets absolute position as 0
+     *  @param speed speed which will be used for homing
+     *  @param accTimeMs acceleration time in ms
+     *  @param dir
+     *  @return false if motor cant be moved rn /e.g is moving to another position/, true if command is accepted
+     */
+    bool home(uint16_t speed,uint16_t accTimeMs,bool dir);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
 #include "DendoStepper.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
 DendoStepper_config_t conf={
     .step_p=14,
     .dir_p=27,
     .en_p=26,
+    .endSw_p=25,
     .timer_group=TIMER_GROUP_0,
     .timer_idx=TIMER_0,
 };
 
 DendoStepper step(&conf);
 
+void task(void*p){
+    while(1){
+        vTaskDelay(1000);
+        if(step.getState()<=IDLE){
+            if(step.home(1000,1000,CCW)){
+            ESP_LOGI("homing","now");
+            vTaskDelete(NULL);
+        }
+        }
+    }
+}
+
 extern "C" void app_main() {
     step.init();
-    step.setSpeed(300,5000);
-    step.runPos(100000);
+    step.setSpeed(1000,1000);
+    //step.runPos(-1000);
+    step.runAbsolute(2500);
+    xTaskCreate(task,"stepper",4096,NULL,10,NULL);
 }
