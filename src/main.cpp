9fe00f484e5d6ba466b0037fb06637b8fc64b6d8
Denis <denis.voltmann@gmail.com>
2021-06-15 23:25:04 +0200

    c

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,47 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 
 DendoStepper_config_t conf={
     .step_p=14,
     .dir_p=27,
     .en_p=26,
-    .endSw_p=25,
+    .endSw_p=ENDSW_DISABLED,
     .timer_group=TIMER_GROUP_0,
     .timer_idx=TIMER_0,
 };
 
+DendoStepper_config_t conf1={
+    .step_p=13,
+    .dir_p=12,
+    .en_p=22,
+    .endSw_p=ENDSW_DISABLED,
+    .timer_group=TIMER_GROUP_0,
+    .timer_idx=TIMER_1,
+};
+
 DendoStepper step(&conf);
+DendoStepper step1(&conf1);
 
 void task(void*p){
     while(1){
-        vTaskDelay(1000);
         if(step.getState()<=IDLE){
-            if(step.home(1000,1000,CCW)){
-            ESP_LOGI("homing","now");
-            vTaskDelete(NULL);
+            step.runAbsolute(esp_random()>>18);
         }
+        if(step1.getState()<=IDLE){
+            step1.runAbsolute(esp_random()>>18);
         }
+        vTaskDelay(1000);
     }
 }
 
 extern "C" void app_main() {
     step.init();
     step.setSpeed(1000,1000);
-    //step.runPos(-1000);
-    step.runAbsolute(2500);
+    step1.init();
+    step1.setSpeed(1000,1000);
+    /*step.runPos(-1000);
+    step.runAbsolute(2500);*/
     xTaskCreate(task,"stepper",4096,NULL,10,NULL);
 }
