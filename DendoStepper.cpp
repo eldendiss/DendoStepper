d9c5a9fc055fd07838bf2ca46adfbd0d92f1c158
eldendiss <denis.voltmann@gmail.com>
2023-12-05 11:10:07 +0100

    Cherry pick commit e4a4dafe0ae8a5ba80c21962ae9d6ce697f8e565 from branch master

--- DendoStepper.cpp
+++ DendoStepper.cpp
@@ -223,9 +223,19 @@ uint16_t DendoStepper::getSpeed()
     return ctrl.speed;
 }
 
+uint16_t DendoStepper::getTargetSpeed()
+{
+    return ctrl.targetSpeed;
+}
+
 float DendoStepper::getAcc()
 {
     return ctrl.acc;
+}
+
+float DendoStepper::getDec()
+{
+    return ctrl.dec;
 }
 
 void DendoStepper::stop()
